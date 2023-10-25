753c1bcde73a9f85c3b85f93834c80c2e26f7c8e
Alessio Chessa <ale.chessa28@gmail.com>
2023-10-25 23:14:48 +0200

    setup selenium for ui integration testing, added service page tests

---
+++ frontend/tests/integration.test.js
@@ -0,0 +1,31 @@
+const { Builder, By } = require("selenium-webdriver");
+
+beforeAll(async () => {
+  driver = await new Builder().forBrowser("chrome").build();
+});
+
+describe("Test page to select service", () => {
+  test("Should retrieve service cards", async () => {
+    await driver.get("http://localhost:3000");
+    await driver.manage().setTimeouts({ implicit: 5000 });
+
+    const cards = await driver.findElements(By.id("service-card"));
+    expect(cards).not.toBeNull();
+    expect(cards.length).toBeGreaterThan(0);
+  });
+
+  test("Should redirect to the ticket page when a service card is clicked", async () => {
+    await driver.get("http://localhost:3000");
+    await driver.manage().setTimeouts({ implicit: 5000 });
+
+    const cards = await driver.findElements(By.id("service-card"));
+    await cards[0].click();
+    const currentUrl = await driver.getCurrentUrl();
+    expect(currentUrl).not.toEqual("http://localhost:3000/");
+    console.log(currentUrl);
+  });
+});
+
+afterAll(async () => {
+  await driver.quit();
+});
