2c03aea44ff90e69ba29e039b87f68fc5c38351d
laurall974 <laura.llinares@edu.devinci.fr>
2023-10-25 10:40:15 +0200

    test update

--- backend/tests/ticket.test.js
+++ backend/tests/ticket.test.js
@@ -10,17 +10,17 @@ describe('getAllTickets function', () => {
     it('should return all tickets from db', async () => {
         db.query.mockResolvedValue({ 
             rows: [
-                { number: 1, status: 1, service_id: 1 },
-                { number: 2, status: 0, service_id: 2 },
-                { number: 3, status: 2, service_id: 3 },
-                { number: 4, status: 1, service_id: 3 }
+                { number: 1, status: 1, service_id: 1, date: undefined, counter_number: 10},
+                { number: 2, status: 0, service_id: 2, date: undefined, counter_number: 11 },
+                { number: 3, status: 2, service_id: 3, date: undefined, counter_number: 12 },
+                { number: 4, status: 1, service_id: 3, date: undefined, counter_number: 13 }
             ]
         });
         const expectedTickets = [
-            new Ticket(1, 1, 1),
-            new Ticket(2, 0, 2),
-            new Ticket(3, 2, 3),
-            new Ticket(4, 1, 3)
+            new Ticket(1, 1, date = undefined, 1, 10),
+            new Ticket(2, 0, date = undefined,2, 11),
+            new Ticket(3, 2, date = undefined,3, 12),
+            new Ticket(4, 1, date = undefined,3, 13)
         ];
         expect(await dao.getAllTickets()).toEqual(expectedTickets);
         expect(db.query).toHaveBeenCalledTimes(1);
