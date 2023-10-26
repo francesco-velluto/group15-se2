ab9ad5002d1d0a901b3e0dffbd5cfde56530a1b4
laurall974 <laura.llinares@edu.devinci.fr>
2023-10-26 02:17:44 +0200

    update backend test

--- backend/tests/ticket.test.js
+++ backend/tests/ticket.test.js
@@ -6,6 +6,10 @@ jest.mock('../dao/db', () => ({
     query: jest.fn()
 }));
 
+beforeEach(()=>{
+    jest.resetAllMocks()
+})
+
 describe('getAllTickets function', () => {
     it('should return all tickets from db', async () => {
         db.query.mockResolvedValue({ 
@@ -26,7 +30,7 @@ describe('getAllTickets function', () => {
         expect(db.query).toHaveBeenCalledTimes(1);
     });
 
-    it('should throw an error if db query fails', async () => {
+    it('should handle errror ', async () => {
         db.query.mockRejectedValue('erroooor');
         try {
             await dao.getAllTickets();
@@ -35,3 +39,87 @@ describe('getAllTickets function', () => {
         }
     });
 });
+
+
+
+
+
+describe('getLastTicketNumberByService function', () => {
+    it('should return 0 if no tickets exist', async () => {
+        db.query.mockResolvedValue({ rows: [{ max: null }] });
+        expect(await dao.getLastTicketNumberByService()).toBe(0);
+      });
+    
+      it('should return the maximum ticket number', async () => {
+        db.query.mockResolvedValueOnce({ rows: [{ max: 5 }] });
+    
+        expect(await dao.getLastTicketNumberByService()).toBe(5);
+      });
+
+    it('should handle error', async () => {
+        db.query.mockRejectedValue('erroooor');
+        try {
+            await dao.getLastTicketNumberByService();
+        } catch (err) {
+            expect(err).toEqual('erroooor');
+        }
+    });
+});
+
+
+
+
+  
+
+  describe('getTicketDetails function', () => {
+    it('should return ticket details', async () => {
+        db.query.mockResolvedValue({ 
+            rows: [{ number: 1, status: 1, service_id: 1, date: undefined, counter_number: 10} ]});
+        const expectedTicket = [
+            new Ticket(1, 1, date = undefined, 1, 10)];
+        expect(await dao.getAllTickets()).toEqual(expectedTicket);
+        expect(db.query).toHaveBeenCalledTimes(1);
+    });
+
+    it('should handle errror ', async () => {
+        db.query.mockRejectedValue('erroooor');
+        try {
+            await dao.getAllTickets();
+        } catch (err) {
+            expect(err).toEqual('erroooor');
+        }
+    });
+});
+
+
+
+
+
+
+describe('controlTicketNumber function', () => {
+    it('should return exists: false a ticket with the given number does not exist', async () => {
+      const mockTicketNumber = 1;
+      db.query.mockResolvedValueOnce( {rows: [{ count: 0 }] });
+  
+      const result = await dao.controlTicketNumber(mockTicketNumber);
+      expect(result.exists).toBe(false);
+    });
+  
+    it('should return exists: true if tickets with the given number exist', async () => {
+      const mockTicketNumber = 1;
+      db.query.mockResolvedValueOnce({ rows: [{ count: 1 }] });
+  
+      const result = await dao.controlTicketNumber(mockTicketNumber);
+      expect(result.exists).toBe(true);
+    });
+  
+    it('should handle errror ', async () => {
+        db.query.mockRejectedValue('erroooor');
+        try {
+            await dao.controlTicketNumber();
+        } catch (err) {
+            expect(err).toEqual('erroooor');
+        }
+    });
+  });
+  
